80010950e8a8e7ddea679c61c47a1c91e7d9f467
lei-chieh <lei.chieh@gmail.com>
2015-09-11 09:54:47 +0800

    加入假畫面

---
+++ Work.WebProj/Controllers/FakeController.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using ProcCore.HandleResult;
+using DotWeb.CommSetup;
+using DotWeb.Controller;
+namespace DotWeb.Controllers
+{
+    public class FakeController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View("Birth");
+        }
+
+        public ActionResult Birth()
+        {
+            return View("Birth");
+        }
+        public ActionResult Birth2()
+        {
+            return View("Birth2");
+        }
+
+        public ActionResult Sell()
+        {
+            return View("Sell");
+        }
+        public ActionResult Sell2()
+        {
+            return View("Sell2");
+        }
+
+        public ActionResult Require()
+        {
+            return View("Require");
+        }
+        public ActionResult Require2()
+        {
+            return View("Require2");
+        }
+
+        public ActionResult Schedule()
+        {
+            return View("Schedule");
+        }
+
+        public ActionResult Dishes()
+        {
+            return View("Dishes");
+        }
+        public ActionResult Dishes2()
+        {
+            return View("Dishes2");
+        }
+        public ActionResult Dishes3()
+        {
+            return View("Dishes3");
+        }
+
+        public ActionResult Gift()
+        {
+            return View("Gift");
+        }
+        public ActionResult Gift2()
+        {
+            return View("Gift2");
+        }
+
+        public ActionResult Tel()
+        {
+            return View("Tel");
+        }
+        public ActionResult Tel2()
+        {
+            return View("Tel2");
+        }
+        public ActionResult Tel3()
+        {
+            return View("Tel3");
+        }
+        public ActionResult Tel4()
+        {
+            return View("Tel4");
+        }
+        public ActionResult Tel5()
+        {
+            return View("Tel5");
+        }
+
+        public ActionResult Check()
+        {
+            return View("Check");
+        }
+
+        public ActionResult Report()
+        {
+            return View("Report");
+        }
+        public ActionResult Report2()
+        {
+            return View("Report2");
+        }
+        public ActionResult Report3()
+        {
+            return View("Report3");
+        }
+    }
+}
--- Work.WebProj/Scripts/ts-def/ConsoleIe8.js
+++ Work.WebProj/Scripts/ts-def/ConsoleIe8.js
@@ -8,6 +8,7 @@ if (typeof console === "undefined" || typeof console.log === "undefined") {
     };
     console.error = function (msg) {
     };
-    console.warn = function (msg) { };
+    console.warn = function (msg) {
+    };
 }
 ;
