6b413e7ba55cc1035694b5267ac25ddcefcea22f
lei-chieh <lei.chieh@gmail.com>
2015-09-21 18:37:48 +0800

    假畫面排版

--- Work.WebProj/Controllers/FakeController.cs
+++ Work.WebProj/Controllers/FakeController.cs
@@ -23,62 +23,122 @@ namespace DotWeb.Controllers
         {
             return View("Birth");
         }
+        public ActionResult Birth_2()
+        {
+            return View("Birth_2");
+        }
         public ActionResult Birth2()
         {
             return View("Birth2");
         }
+        public ActionResult Birth2_2()
+        {
+            return View("Birth2_2");
+        }
         public ActionResult Birth3()
         {
             return View("Birth3");
+        }
+        public ActionResult Birth3_2()
+        {
+            return View("Birth3_2");
         }
 
         public ActionResult Sell()
         {
             return View("Sell");
         }
+        public ActionResult Sell_2()
+        {
+            return View("Sell_2");
+        }
         public ActionResult Sell2()
         {
             return View("Sell2");
+        }
+        public ActionResult Sell2_2()
+        {
+            return View("Sell2_2");
+        }
+        public ActionResult Sell2_3()
+        {
+            return View("Sell2_3");
         }
 
         public ActionResult Require()
         {
             return View("Require");
         }
+        public ActionResult Require_2()
+        {
+            return View("Require_2");
+        }
         public ActionResult Require2()
         {
             return View("Require2");
+        }
+        public ActionResult Require2_2()
+        {
+            return View("Require2_2");
         }
 
         public ActionResult Schedule()
         {
             return View("Schedule");
         }
+        public ActionResult Schedule_2()
+        {
+            return View("Schedule_2");
+        }
 
         public ActionResult Dishes()
         {
             return View("Dishes");
         }
+        public ActionResult Dishes_2()
+        {
+            return View("Dishes_2");
+        }
         public ActionResult Dishes2()
         {
             return View("Dishes2");
+        }
+        public ActionResult Dishes2_2()
+        {
+            return View("Dishes2_2");
         }
         public ActionResult Dishes3()
         {
             return View("Dishes3");
         }
+        public ActionResult Dishes3_2()
+        {
+            return View("Dishes3_2");
+        }
         public ActionResult Dishes4()
         {
             return View("Dishes4");
+        }
+        public ActionResult Dishes4_2()
+        {
+            return View("Dishes4_2");
         }
 
         public ActionResult Gift()
         {
             return View("Gift");
         }
+        public ActionResult Gift_2()
+        {
+            return View("Gift_2");
+        }
         public ActionResult Gift2()
         {
             return View("Gift2");
+        }
+        public ActionResult Gift2_2()
+        {
+            return View("Gift2_2");
         }
 
         public ActionResult Tel()
